997e4663822e49f197c22981a0a30861c1f9ee08
Symfonia2023 <Symfonia.gp@gmail.com>
2023-08-14 14:26:21 -0300

    Se agregó la plantilla para establecer conexion a la base de datos

---
+++ acceso a datos/BD_connection.php
@@ -0,0 +1,17 @@
+<?php
+$servername = "";   // Nombre del servidor, "localhost" si se hace en localhost
+$username = "root"; // el usuario predeterminado que se crea en localhost en XAMPP es este, de usuario root y password vacia
+$password = "";
+$database = ""; // el nombre que le hayan puesto a la base de datos
+
+// Crear conexión
+$conn = mysqli_connect($servername, $username, $password, $database);  // Establecer la conexion con la base de datos.
+
+// Verificar conexión
+if (!$conn) {
+    die("Error de conexión: " . mysqli_connect_error());
+} else {
+    echo "La conexion con la base de datos ha sido establecida.";
+}
+
+?>
