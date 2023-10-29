370abd4a63e469b780e29e4659b2d9f846e3566b
Symfonia2023 <Symfonia.gp@gmail.com>
2023-10-28 22:12:16 -0300

    Se modificaron botones y se mejoro la interfaz de los menus preparados y se le añadió un diseño responsive

--- presentacion/js/menusPreparados.js
+++ presentacion/js/menusPreparados.js
@@ -11,13 +11,13 @@ $("#menu-mensual-title").click(function() {
 });
 
 function toggleContenido(tipo) {
-    const sectionMenuContent = $(`.section-menu-${tipo} .sectionMenu-content`);
+    const sectionMenuContent = $(`#section-menu-${tipo} .sectionMenu-content`);
 
     if (sectionMenuContent.css("display") === "none") {
         sectionMenuContent.css("display", "flex");
-        sectionMenuContent.hide().fadeIn(500); // Aplicar fadeIn después de cambiar a display: flex
+        sectionMenuContent.hide().fadeIn(400); // Aplicar fadeIn después de cambiar a display: flex
     } else {
-        sectionMenuContent.fadeOut(500, function() {
+        sectionMenuContent.fadeOut(400, function() {
             sectionMenuContent.css("display", "none");
         });
     }
